a0b7d4db71fba151230c676457e2f5d84942d0c6
Davinia pd <delgadavi@gmail.com>
2024-08-21 19:17:13 +0000

    personalizado el footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+	<footer className="footer mx-auto py-3 text-center bg-warning bg-opacity-25">
+		<p className="my-auto">
+			Made with <i className="fa-solid fa-wand-sparkles text-warning" /> by{" "}
+			<a href="https://github.com/daviniapd" className="text-warning">daviniapd</a> {" "}
+			in{" "}
+			<a href="http://www.4geeksacademy.com" className="text-warning">4Geeks Academy</a>
 		</p>
 	</footer>
 );
