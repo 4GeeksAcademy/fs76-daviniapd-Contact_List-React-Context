680d2282b3a93393d001bd83c847a02accc57239
Davinia pd <delgadavi@gmail.com>
2024-08-25 15:39:08 +0000

    Diseño V2 en construccion

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export const Footer = () => (
-	<footer className="footer fixed-bottom py-3 bg-warning text-center bg-opacity-25 d-flex justify-content-between">
+	<footer className="footer fixed-bottom py-3 bg-light text-center bg-opacity-25 d-flex justify-content-between">
 		<span className="px-5 mx-5"></span>
 		<p className="my-auto ms-5">
-			Made with <i className="fa-solid fa-wand-sparkles text-secondary" /> by{" "}
-			<a href="https://github.com/daviniapd" className="link-secondary">daviniapd</a> {" "}
+			Made by{" "}
+			<a href="https://github.com/daviniapd" className="link-secondary">daviniapd</a> <i className="fa-solid fa-otter text-secondary" /> {" "}
 			in{" "}
-			<a href="http://www.4geeksacademy.com" className="link-secondary">4Geeks Academy</a>
+			<a href="http://www.4geeksacademy.com" className="link-secondary">4Geeks Academy</a> 
 		</p>
 
 		<Link to="/" className="">
--- src/js/views/addContactV2.js
+++ src/js/views/addContactV2.js
@@ -9,8 +9,7 @@ export const AddContactV2 = () => {
 	return (
 		<>
 			<div className="d-flex justify-content-center align-items-center h-100 flex-column m-5">
-	
-				<h1 className="text-center my-3 bg-warning" id="titleHomr">UNDER CONSTRUCTION</h1>
+				<h1 className="text-center my-3 p-5 bg-warning" id="titleHomr"><i class="fa-solid fa-radiation"></i>  UNDER CONSTRUCTION  <i class="fa-solid fa-radiation"></i></h1>
 			</div>
 
 		</>
